c1f9d5d78951e4eafb8ca5ace5c173371495dd44
Zachary Cloutier <zcloutie@uci.edu>
2018-02-28 15:56:28 -0800

    Player Movement and Tether Logic

---
+++ Unity Project/Game 3/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	private Rigidbody2D rb;
+	void Awake(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb.velocity = movement * speed;
+
+		/*rb.position = new Vector2 
+			(
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+				Mathf.Clamp (rb.position.y, boundary.zMin, boundary.zMax)
+			);
+
+		//rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);*/
+	}
+}
---
+++ Unity Project/Game 3/Assets/Tether.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tether : MonoBehaviour {
+
+	public Transform player;
+
+	private LineRenderer lr;
+
+	void Awake(){
+		lr = GetComponent<LineRenderer> ();
+	}
+
+	void FixedUpdate(){
+		lr.SetPosition (1, player.position);
+		RaycastHit2D hit;
+		if (Physics2D.Linecast (transform.position, player.position)) {
+			hit = Physics2D.Linecast (transform.position, player.position);
+			Destroy(hit.collider.gameObject);	
+		}
+	}
+}
