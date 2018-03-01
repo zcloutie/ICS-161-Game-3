aa6736113231c46991e2a1da4df2a9fd12b02eab
Mark Pareja <Markiled@users.noreply.github.com>
2018-03-01 10:45:34 -0800

    Enemies and Particles
    Added basic enemy spawn and follow AI and some particle effects

---
+++ Unity Project/Game 3/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+    GameObject target;
+    public Transform position;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.LookAt(target.transform);
+        transform.Translate(Vector3.forward * 0.5f * Time.deltaTime);
+	}
+}
