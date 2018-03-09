8927b7ee28131aca6ef63d09fb5bb52bb7877954
TinaFeng <xuanfeng1996@gmail.com>
2018-03-08 22:35:13 -0800

    Added chain spawner
    Only looks nice as of now

--- Unity Project/Game 3/Assets/Scripts/Chain_Creator.cs
+++ Unity Project/Game 3/Assets/Scripts/Chain_Creator.cs
@@ -7,27 +7,50 @@ public class Chain_Creator : MonoBehaviour {
 
     public GameObject Player2;
     public GameObject chain_prefab;
-    public int chain_size = 3;
+
+    GameObject[] chains;
+    GameObject chain;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
         GenerateChain();
-	}
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
     void GenerateChain()
     {
-        float distance = Mathf.Abs(Player2.transform.position.x - transform.position.x);
-        float chain_amount = Mathf.Round(distance / chain_size);
-        Vector3 previous = this.transform.position;
-        for (int i = 0; i != chain_amount;i++)
+
+        float distance = Mathf.Abs(Player2.transform.position.x - transform.position.x); //distance between 2 players
+        float chain_size = chain_prefab.GetComponent<SpriteRenderer>().bounds.size.x/2;
+        int chain_amount = Mathf.RoundToInt(distance /chain_size );//how many chains to spawn
+        GameObject previous = this.gameObject;//last chain position
+        Rigidbody2D Last_Chain = this.GetComponent<Rigidbody2D>();//last rigidbody to connect
+        float distance_x = (Player2.transform.position.x - this.transform.position.x) / chain_amount;
+        float distance_y = (Player2.transform.position.y - this.transform.position.y) / chain_amount;
+
+        
+        for (int i = 0; i <= chain_amount;i++)
         {
 
-            GameObject chain = Instantiate(chain_prefab, previous, Quaternion.identity);
-            chain.transform.parent = this.transform;
+            if ((previous.transform.position.x + distance_x) <= Player2.transform.position.x)
+            {
+                chain = Instantiate(chain_prefab, new Vector3(previous.transform.position.x + distance_x, previous.transform.position.y + distance_y), Quaternion.identity);
+
+                previous = chain;
+                chain.transform.parent = this.transform;
+                chain.GetComponent<HingeJoint2D>().connectedBody = Last_Chain;
+                Last_Chain = chain.GetComponent<Rigidbody2D>();
+
+
+            }
+
         }
+        Player2.GetComponent<HingeJoint2D>().connectedBody = Last_Chain;
     }
 }
---
+++ Unity Project/Game 3/Assets/Stay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stay : MonoBehaviour {
+    Vector3 spawn;
+	// Use this for initialization
+	void Start () {
+        spawn = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position = spawn;
+	}
+}
