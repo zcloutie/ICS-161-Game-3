60ee476c529ce8b50cfa5aee4b7e48ac3b50c6c8
Mark Pareja <Markiled@users.noreply.github.com>
2018-03-15 18:20:27 -0700

    SFX and More
    see title

--- Unity Project/Game 3/Assets/Scripts/Chain_Behavior.cs
+++ Unity Project/Game 3/Assets/Scripts/Chain_Behavior.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chain_Behavior : MonoBehaviour {
+    public AudioClip hit;
 
-
-	// Use this for initialization
-	void Start () {
-
+    // Use this for initialization
+    void Start () {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = hit;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
      
         if (collision.gameObject.tag == "Enemy"&& GetComponent<SpriteRenderer>().enabled == true)
         {
-
+            GetComponent<AudioSource>().Play();
             Destroy(collision.gameObject);
         }
 
